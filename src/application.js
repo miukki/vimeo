3e83db14a60b3b0a9a52a12644161f8217c93dd4
miukki macbook air <miukki@gmail.com>
2012-06-04 00:01:30 +0400

    backbone

--- src/application.js
+++ src/application.js
@@ -10,12 +10,34 @@
             error: options.error
         };
 
-        if (model.model == AlbumsModel && method == 'read') {
+        if (method == 'read') {
             params.data = {
                 method: "albums.getAll", 
                 params: {"user_id": options.userId}
             }
-        }
+        };
+        
+        if (method == 'delete') {
+            params.data = {
+                method: "albums.delete", 
+                params: {
+                    album_id: model.id,
+                }
+            }
+        };
+
+        if (method == 'create') {
+            params.data = {
+                method: "albums.create", 
+                params: {
+                    video_id: model.get('video_id'),
+                    title: model.get('title'),
+                    description: model.get('description')
+                }
+            }
+        };
+
+        
         return $.ajax(params);
     };
     
@@ -23,10 +45,12 @@
         defaults: {
             title: 'title default',
         },
-        validate: function(){
+        validate: function(attr){
+            
         },
         initialize: function(){
-        }
+        }        
+
     });
     
     AlbumsCollection = Backbone.Collection.extend({
@@ -34,55 +58,101 @@
         initialize: function(){},
         silent: true,
         parse: function(resp, xhr) {
-            console.log(resp.albums.album);
             return resp.albums.album;
         }
     });
     
-    var myAlbumsCollection = new AlbumsCollection;
-
+    myAlbumsCollection = new AlbumsCollection;
     
+    CreateAlbumView = Backbone.View.extend({
+        template: _.template($('#myCreateAlbumViewTmpl').html()),
+        initialize: function(){
+            this.render();
+        },
+        render: function(){
+           $(this.el).empty();
+           $(this.el).html(this.template({}));
+        },
+        events: {
+            "click .createAlbumSubmit": "createAlbumSubmit"
+        },
+        createAlbumSubmit: function(){
+            //console.log(this.$el.find('#id').val());
+            this.model.set({title: this.$el.find('#title').val(), description: this.$el.find('textarea').val(), video_id: this.$el.find('#id').val()});
+            console.log(this.model.isNew());
+            this.model.save({success: reloadAlbums});
+        }        
+    });
+
+    AlbumView = Backbone.View.extend({
+        template: _.template($('#myAlbumViewTmpl').html()),
+        initialize: function(){
+            this.render();
+        },
+        render: function(){
+           $(this.el).empty();
+           $(this.el).html(this.template(this.model.toJSON()));
+        },
+        events: {
+           "click .deleteAlbum": "deleteAlbum",
+           "click .editAlbum": "editAlbum",
+           "click .viewAlbum": "viewAlbum",
+           "click .addVideo": "addVideo"
+        },
+        deleteAlbum: function(){
+            var that = this;
+            this.model.destroy({success: function(model, response){
+                that.trigger("deleted");
+            }});
+        },
+        editAlbum: function(event){
+           console.log('editAlbum');
+        },
+        viewAlbum: function(event){
+           console.log('viewAlbum');
+        },
+        addVideo: function(event){
+           console.log('addVideo');
+        }
+    });
     
     AlbumsView = Backbone.View.extend({
-            template: _.template($('#myAlbumViewTmpl').html()),
+            template: _.template($('#AllAblumsViewTmpl').html()),
             initialize: function(){
                 this.render();
+                this.collection.on("change", this.render, this)
             },
             render: function(){
-                var variables = {};
                 $(this.el).empty();
-                _.each(this.collection.toJSON(), function(album) {
-                    album.thumbnail = album.thumbnail_video.thumbnails.thumbnail[1]._content;
-                    $(this.el).append($(this.template(album)));
+                $(this.el).html(this.template({}));
+                _.each(this.collection.models, function(album) {
+                    console.log('album', album);
+                    var $div = $('<div></div>');
+                    $(this.el).append($div);
+                    var myAlbumView = new AlbumView({ model: album, el: $div });
+                    myAlbumView.on("deleted", this.render, this);
                 }, this); 
             },
             events: {
-                "click .deleteAlbum": "deleteAlbum",
-                "click .editAlbum": "editAlbum",
-                "click .viewAlbum": "viewAlbum",
-                "click .addVideo": "addVideo"
+                "click .createAlbum": "createAlbum"
             },
-            deleteAlbum: function(event){
-                console.log( "deleteAlbum");
-            },
-            editAlbum: function(event){
-                console.log( "editAlbum");
-            },
-            viewAlbum: function(event){
-                console.log( "viewAlbum");
-            },
-            addVideo: function(event){
-                console.log( "addVideo");
+            createAlbum: function(){
+               var myCreateAlbumView = new CreateAlbumView({ model: new AlbumsModel, el: $('#myCreateAlbumViewHtml') });
             }
+            
         });    
         
-    myAlbumsCollection.fetch({
-        userId: userId,
-        success: function(){
-            console.log("myAlbumsCollection.fetch succeeded")
-            var myAlbumsView = new AlbumsView({ collection: myAlbumsCollection, el: $('#myAlbumsViewHtml') });
-        }
-    });
+    function reloadAlbums(){
+        myAlbumsCollection.fetch({
+            userId: userId,
+            success: function(){
+                console.log("myAlbumsCollection.fetch succeeded")
+                var myAlbumsView = new AlbumsView({ collection: myAlbumsCollection, el: $('#AllAblumsViewHtml') });
+            }
+        });        
+    };
+    
+    reloadAlbums();
         
     AppRouter = Backbone.Router.extend({
          routes: {
